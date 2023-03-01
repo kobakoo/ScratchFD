b366543d0aa9a65292c84a9b931d524b8a4aa6e6
Kobako <100463101+kobakoo@users.noreply.github.com>
2023-03-01 16:32:53 +0900

    commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/kobakoo/ScratchFD/docs/",
+          editUrl: "https://github.com/kobakoo/ScratchFD/",
         },
         blog: {
           showReadingTime: true,
